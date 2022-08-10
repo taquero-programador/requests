bf4ec80d69902602f95ace108a663956973512be
taquero-programador <jrangel93@protonmail.com>
2022-08-10 15:28:29 -0500

    feat(OAuth)

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import requests
-
 from requests_oauthlib import OAuth1
 
 url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 
-auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
-
-              'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET', 'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
 
 r = requests.get(url, auth=auth)
 print(r) # response 200

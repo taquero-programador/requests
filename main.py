3be4d793bf1fc8f5cde43f511557e52e17edd7bf
taquero-programador <jrangel93@protonmail.com>
2022-08-10 15:26:52 -0500

    feat(OAuth)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-
 import requests
-import json
-from requests.auth import HTTPBasicAuth
 
-auth = HTTPBasicAuth('taquero-programador', 'no_pass')
-body = ({u"body": u"Sounds great! I'll get right on it!"})
-url = u"https://api.github.com/repos/psf/requests/issues/482/comments"
-r = requests.post(url, data=body, auth=auth)
-print(r.status_code)
+from requests_oauthlib import OAuth1
+
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+
+auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
+
+              'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+
+r = requests.get(url, auth=auth)
+print(r) # response 200
+
--- note.py
+++ note.py
@@ -497,6 +497,41 @@ print(r.headers)
 'x-ratelimit-remaining': '4995'
 'x-ratelimit-limit': '5000'
 
+# Autenticacion
+# autenticacion básica. hacer solicitudes con HTTPBasicAuth
+from requests.auth import HTTPBasicAuth
+
+basic = HTTPBasicAuth('user', 'pass')
+url = 'https://httpbin.org/basic-auth/user/pass'
+r = requests.get(url, auth=basic)
+print(r.text) # retorna un dict con los valores de auth
+print(r.status_code) # retorna 200, exito
+# pasar directo como argumento de tipo tupla
+requests.get(url, auth=('user', 'pass'))
+"""
+autenticación netrc
+el archivo netrc anula los encabezados de autenticación HTTP sin establecer el formato headers=
+"""
+# autenticación implicita. autenticacion digest
+import requests
+from requests.auth import HTTPDigestAuth
+
+url = 'https://httpbin.org/digest-auth/auth/user/pass'
+r = requests.get(url, auth=HTTPDigestAuth('user', 'pass'))
+print(r) # <Response [401]>
+print(r.status_code)
+# autenticacion OAuth1. una forma común de auth en APIS es OAuth
+import requests
+from requests.oauthlib import OAuth1
+
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
+ 'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+r = requests.get(url, auth=auth)
+print(r) # reponse 200
+
+
+
 
 
 # ------------------------------------------------------
